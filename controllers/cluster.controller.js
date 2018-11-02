3155bc5573eacc6bcd2a9da3a911cd8dea3ee86b
neelimavs <neelima@krikey.com>
2018-11-01 20:25:40 -0700

    Final Commit

--- controllers/cluster.controller.js
+++ controllers/cluster.controller.js
@@ -9,7 +9,7 @@ function mean_value(values) {
 // A custom mode function which will return mode values only if it is 1 or 0.
 // When 0, we check if all the records are the same which means that can be used as mode, else returns 0.
 function mode_value(values) {
-    if (values.length < config.MINIMUM_TRANSACTIONS_FOR_RECCURING) return 0;
+    if (values.length < config.MINIMUM_TRANSACTIONS_FOR_RECURRING) return 0;
     modes = Array.from(stats.mode(values));
     if (modes.length == 0 && (values[0] == values[1])) return values[0];
     else if (modes.length == 1) return modes;
@@ -26,7 +26,7 @@ function getDiff(current, last) {
 }
 
 // To find the next occurance of a particular transaction,
-// we add the last date with the reccuring peroid.
+// we add the last date with the recurring peroid.
 function addDays(date, days) {
     var result = new Date(date);
     result.setDate(result.getDate() + days);
@@ -34,33 +34,37 @@ function addDays(date, days) {
 }
 
 // The user+company group is taken as parameter by this function and
-// this will remove the outlier transactions to group all reccuring transactions
+// this will remove the outlier transactions to group all recurring transactions
 // and to find the next potential date and amount of the user's transaction.
 exports.apply_cluster = function (user_groups) {
-    var final_reccuring_list = [];
+    var final_recurring_list = [];
     _.forEach(user_groups, function (company_groups) {
         _.forEach(company_groups, function (obj_arr) {
             obj_arr[0].days = 0
             for (var i = 1; i < obj_arr.length; i++) {
                 obj_arr[i].days = getDiff(obj_arr[i].date, obj_arr[i - 1].date);
             }
-            docs = _.filter(obj_arr, function (o) { return o.days != 0 });
+            docs = _.filter(obj_arr, function (o) {
+                return o.days != 0
+            });
             var mode = mode_value(_.map(docs, 'days'));
             if (mode > 0) {
-                let all_txns = _.filter(docs, function (obj) { return obj.days == mode });
+                let all_txns = _.filter(docs, function (obj) {
+                    return obj.days == mode
+                });
                 if (all_txns.length > 0) {
-                    let last_date = _.sortBy(all_txns, o => o.date)[all_txns.length - 1].date;
+                    let last_txn = _.sortBy(all_txns, o => o.date)[all_txns.length - 1];
                     let output = {
-                        user_id: all_txns[0].user_id,
-                        name: all_txns[0].name,
-                        next_amt: mean_value(_.map(all_txns, 'amount')),    // Mean of all reccuring txns will give next probable txn amount.
-                        next_date: addDays(last_date, mode),   // Adding the mode value of days with the last txn date will give next txn date. 
-                        txns: all_txns
+                        name:last_txn.name,
+                        user_id: last_txn.user_id,
+                        next_amt: mean_value(_.map(all_txns, 'amount')), // Mean of all recurring txns will give next probable txn amount.
+                        next_date: addDays(last_txn.date, mode), // Adding the mode value of days with the last txn date will give next txn date. 
+                        transactions: all_txns
                     }
-                    final_reccuring_list.push(output);
+                    final_recurring_list.push(output);
                 }
             }
         });
     });
-    return _.sortBy(final_reccuring_list, o => o.user_id);
+    return _.sortBy(final_recurring_list, o => o.name);
 }
